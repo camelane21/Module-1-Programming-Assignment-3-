149d28daafb4362d21081ef63dc82cf9a29cd97d
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-06-15 15:45:07 -0400

    Update Main.java
    try

--- Main.java
+++ Main.java
@@ -13,3 +13,33 @@ public class Exercise08_29 {
         }
         return true;
     }
+
+      /** Print a 3×3 matrix on one line, space‐separated */
+    private static void printMatrix(int[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.print(m[i][j]);
+                if (!(i == m.length - 1 && j == m[i].length - 1)) {
+                    System.out.print(" ");
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter m1 (a 3 by 3 matrix) row by row: ");
+        int[][] m1 = new int[3][3];
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                m1[i][j] = input.nextInt();
+
+        System.out.print("Enter m2 (a 3 by 3 matrix) row by row: ");
+        int[][] m2 = new int[3][3];
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                m2[i][j] = input.nextInt();
+        
+                
